43b0e86d718fbe47d86c2226482eb93776c1f401
Abdulrahmanmoin <iam.armoin@gmail.com>
2024-11-06 20:30:49 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Addtodo from './components/Addtodo'
+import Todo from './components/Todo'
+
+function App() {
+
+  return (
+    <>
+      <Addtodo />
+      <Todo />
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import { store } from './app/store.js'
+import {Provider} from 'react-redux'
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+)
